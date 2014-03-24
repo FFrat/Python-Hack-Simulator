47ce32aff1d7f7df2a75baaf4e2241ec349f8b4f
dafull97 <dafull97@gmail.com>
2014-03-24 10:07:33 -0400

    Created V1
    nope nothing

---
+++ Simulator-windowsV1.py
@@ -0,0 +1,18 @@
+import time, os, socket
+
+##Dont hack anything, just a "Animated Movie"
+
+target = raw_input("Targeted Computer: ")
+os.system("cls")
+raw_input("Type of Hack - Core, CPU: ")
+os.system("cls")
+print "HACK STARTED",target
+time.sleep(5)
+print """
+MAIN FRAME DROPPED
+"""
+time.sleep(5)
+print """
+injecting into the Mainframe...
+"""
+print(type)
